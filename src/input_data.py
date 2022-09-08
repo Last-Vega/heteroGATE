62260b0badfabb8d032b1a66c84c36bc29d6352e
Last-Vega <watanabe.shingo.ss@alumni.tsukuba.ac.jp>
2022-09-08 23:01:31 +0900

    Update input_data.py

--- src/input_data.py
+++ src/input_data.py
@@ -3,23 +3,36 @@ from torch_geometric.data import HeteroData
 from torch_geometric.loader import NeighborLoader
 import torch_geometric.transforms as T
 import torch
+import numpy as np
 
 def load_binary(path):
     with open(f'{path}', 'rb') as rb:
         data = pickle.load(rb)
     return data
 
+def createMetaPathBasedAdj(m1, m2):
+    adj = np.matmul(m1, m2)
+    adj[adj > 1] = 1
+    return adj
+
 name_list = ['kato']
 
+# author行列
+A = load_binary('../data/kato/author/author_1.Matrix').toarray()
+# venue行列
+V = load_binary('../data/kato/conference/conference_1.Matrix').toarray()
+# keyword行列
+K = load_binary('../data/kato/term/term_1.Matrix').toarray()
+# year行列
+Y = load_binary('../data/kato/year/year_1.Matrix').toarray()
+
+PAP = createMetaPathBasedAdj(A, A.T)
+APA = createMetaPathBasedAdj(A.T, A)
+
 data = HeteroData()
+data['author'] = torch.tensor(A)
+data['venue'] = torch.tensor(V)
+data['keyword'] = torch.tensor(K)
+data['year'] = torch.tensor(Y)
 
-A = load_binary('../data/kato/author/author_1.Matrix')
-V = load_binary('../data/kato/conference/conference_1.Matrix')
-S = load_binary('../data/kato/term/term_1.Matrix')
-Y = load_binary('../data/kato/year/year_1.Matrix')
-
-print(A.shape)
-print(V.shape)
-
-# data['key'] = torch.tensor()
-
+print(data)
